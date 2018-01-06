eedb4279f9a97acc3a5859600140a13a50e39141
stla <laurent_step@yahoo.fr>
2018-01-06 07:07:13 +0100

    vector space

--- README.md
+++ README.md
@@ -4,31 +4,72 @@ Get the vertices of a polyhedron defined by linear constraints.
 
 ## Example
 
-Define the variables:
+Consider the system of linear constraints:
 
-```haskell
-x = newVar 1
-y = newVar 2
-z = newVar 3
-vars = [x,y,z]
+```
+x + y + z =  0
+        z <= 10
+x + y     <= 5/2
+x - y     <= 0
+x - y     >= -1        
 ```
 
-Define a set of linear constraints:
+First define the variables:
+
+```haskell
+x = newVar 1 -- that means x is the first variable
+y = newVar 2 -- y is the second variable
+z = newVar 3 -- z is the third variable
+```
+
+Define the set of linear constraints:
 
 ```haskell
 import Data.Ratio
+vars = [x,y,z]
 constraints =
   [(linearCombination vars [(1,x), (1,y), (1,z)]) .=. constant 0
   ,(linearCombination vars [(1,z)])               .<. constant 10
-  ,(linearCombination vars [(1,x), (-1,y)])       .>. constant (-10)
   ,(linearCombination vars [(1,x), (1,y)])        .<. constant (5%2)
   ,(linearCombination vars [(1,x), (-1,y)])       .<. constant 0
-  ,(linearCombination vars [(1,x), (-1,y)])       .>. constant (-1)]
+  ,(linearCombination vars [(2,x), (-1,y)])       .>. constant (-1)]
 ```
 
 Get the vertices:
 
 ```haskell
-> getVertices constraintsExample
-[[(-5) % 1,(-5) % 1,10 % 1],[5 % 4,5 % 4,(-5) % 2],[3 % 4,7 % 4,(-5) % 2],[(-11) % 2,(-9) % 2,10 % 1]]
+> vertices <- getVertices constraints
+> vertices
+[[5 % 4,5 % 4,(-5) % 2],[(-1) % 1,(-1) % 1,2 % 1],[1 % 2,2 % 1,(-5) % 2]]
+```
+
+If you find this output unreadable, use `showVertices`:
+
+```haskell
+> showVertices vertices
+(5/4, 5/4, -5/2)
+(-1, -1, 2)
+(1/2, 2, -5/2)
+```
+
+The type of the two members of a linear constraint is `LinearCombination`,
+and this type has the `VectorSpace` instance. Thus the above set of constraints
+can be equivalently defined like this:
+
+```haskell
+import Data.VectorSpace
+x = newVar 1
+y = newVar 2
+z = newVar 3
+vars = [x,y,z]
+x' = asLinearCombination vars x
+y' = asLinearCombination vars y
+z' = asLinearCombination vars z
+one = constant 1
+constraints =
+  [ (x' ^+^ y' ^+^ z') .=. zeroV
+  , z'                 .<. (10*^one)
+  , (x' ^+^ y')        .<. (5*^one ^/ 2)
+  , (x' ^-^ y')        .<. zeroV
+  , (2*^x' ^-^ y')     .>. ((-1)*^one)]
 ```
--- src/Vertices.hs
+++ src/Vertices.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE TypeFamilies             #-}
 module Vertices
-  (newVar, linearCombination, constant, (.>.), (.<.), (.=.), getVertices)
+  (newVar, linearCombination, constant, (.>.), (.<.), (.=.), getVertices, showVertices)
   where
+import           Data.AdditiveGroup
 import           Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as M
 import           Data.List          (intercalate)
 import           Data.List.Unique   (allUnique)
 import           Data.Ratio
 import           Data.Tuple         (swap)
+import           Data.VectorSpace
 import           Foreign.C.String
 import           System.IO          (writeFile)
 import           TemporaryFile
@@ -15,6 +18,12 @@ import           Text.Printf        (printf)
 
 foreign import ccall unsafe "getVertices" c_getVertices
   :: CString -> IO CString
+
+showRational :: Rational -> String
+showRational r = if q==1 then show p else (show p) ++ "/" ++ (show q)
+                where
+                  p = numerator r
+                  q = denominator r
 
 type Var = Int
 
@@ -24,14 +33,22 @@ data LinearCombination = LinearCombination (IntMap Rational)
 instance Show LinearCombination where
   show (LinearCombination x) =
     intercalate " + " $
-     (map (\(i,r) -> if i==0 then showR r else (showR r) ++ " x" ++ (show i))
+     (map (\(i,r) -> if i==0
+                     then showRational r
+                     else (showRational r) ++ " x" ++ (show i))
           (M.toAscList x))
 
-showR :: Rational -> String
-showR r = if q==1 then show p else (show p) ++ "/" ++ (show q)
-          where
-            p = numerator r
-            q = denominator r
+instance AdditiveGroup LinearCombination where
+  zeroV = LinearCombination (M.singleton 0 0)
+  (^+^) (LinearCombination imap1) (LinearCombination imap2) =
+    LinearCombination
+    (M.mergeWithKey (\i -> \x -> \y -> Just (x+y)) id id imap1 imap2)
+  negateV (LinearCombination imap) = LinearCombination (M.map negate imap)
+
+instance VectorSpace LinearCombination where
+  type Scalar LinearCombination = Rational
+  (*^) lambda (LinearCombination imap) =
+    LinearCombination (M.map (*lambda) imap)
 
 data Sense = Gt | Lt | Eq
      deriving Eq
@@ -45,7 +62,8 @@ data Constraint = Constraint (LinearCombination, Sense, Rational)
      deriving Eq
 
 instance Show Constraint where
-  show (Constraint (a,b,c)) = (show a) ++ " " ++ (show b) ++ " " ++ (showR c)
+  show (Constraint (a,b,c)) =
+    (show a) ++ " " ++ (show b) ++ " " ++ (showRational c)
 
 newVar :: Int -> Var
 newVar i = case i >= 0 of
@@ -68,6 +86,9 @@ linearCombination vars0 terms =
     simplifiedTerms = simplifyTerms terms
     presentVariables = M.keys (M.delete 0 simplifiedTerms)
     vars = filter (/= 0) vars0
+
+asLinearCombination :: [Var] -> Var -> LinearCombination
+asLinearCombination vars var = linearCombination vars [(1,var)]
 
 constant :: Rational -> LinearCombination
 constant x = LinearCombination (M.singleton 0 x)
@@ -151,6 +172,11 @@ getVertices constraints = do
         False -> return $ rawResultsToVertices rawResults
   where variablesSets = map constraintVariables constraints
 
+showVertices :: [[Rational]] -> IO ()
+showVertices vertices =
+  putStrLn $ intercalate "\n" $
+    map (\v -> "(" ++ (intercalate ", " (map showRational v)) ++ ")") vertices
+
 constraintExample :: Constraint
 constraintExample = l1 .>. l2
   where
@@ -171,7 +197,25 @@ constraintsExample = [c1,c2,c3,c4,c5,c6]
     c3 = linearCombination vars [(1,x), (-1,y)] .>. constant (-10)
     c4 = linearCombination vars [(1,x), (1,y)] .<. constant (5%2)
     c5 = linearCombination vars [(1,x), (-1,y)] .<. constant 0
-    c6 = linearCombination vars [(1,x), (-1,y)] .>. constant (-1)
+    c6 = linearCombination vars [(2,x), (-1,y)] .>. constant (-1)
+
+constraintsExample' :: [Constraint]
+constraintsExample' =
+  [ (x' ^+^ y' ^+^ z') .=. zeroV
+  , z'                 .<. (10*^one)
+  , (x' ^-^ y')        .>. ((-10)*^one)
+  , (x' ^+^ y')        .<. (5*^one ^/ 2)
+  , (x' ^-^ y')        .<. zeroV
+  , (2*^x' ^-^ y')        .>. ((-1)*^one)]
+  where
+    x = newVar 1
+    y = newVar 2
+    z = newVar 3
+    vars = [x,y,z]
+    x' = asLinearCombination vars x
+    y' = asLinearCombination vars y
+    z' = asLinearCombination vars z
+    one = constant 1
 
 constraintsExample2 :: [Constraint]
 constraintsExample2 =
@@ -187,3 +231,52 @@ constraintsExample2 =
     y = newVar 2
     z = newVar 3
     vars = [x,y,z] -- or [c,x,y,z]
+
+constraintsExample3 :: [Constraint]
+constraintsExample3 = -- same as example2 using AdditiveGroup
+  [(linearCombination vars [(1,x)]) .>. constant (-5)
+  ,(linearCombination vars [(1,x)]) .<. constant 4
+  ,(linearCombination vars [(1,y)]) .>. constant (-5)
+  ,(linearCombination vars [(1,y)]) .<. (constant 3 ^-^ linearCombination vars [(1,x)])
+  ,(linearCombination vars [(1,z)]) .>. constant (-10)
+  ,(linearCombination vars [(1,z)]) .<. (constant 6 ^-^ linearCombination vars [(1,x), (1,y)])]
+  where
+    x = newVar 1
+    y = newVar 2
+    z = newVar 3
+    vars = [x,y,z]
+
+constraintsExample4 :: [Constraint]
+constraintsExample4 = -- same as example 2 and 3
+  [ x' .>. constant (-5)
+  , x' .<. constant 4
+  , y' .>. constant (-5)
+  , y' .<. (constant 3 ^-^ x')
+  , z' .>. constant (-10)
+  , z' .<. (constant 6 ^-^ (x' ^+^ y'))]
+  where
+    x = newVar 1
+    y = newVar 2
+    z = newVar 3
+    vars = [x,y,z]
+    x' = asLinearCombination vars x
+    y' = asLinearCombination vars y
+    z' = asLinearCombination vars z
+
+constraintsExample5 :: [Constraint]
+constraintsExample5 = -- same as example 2 3 4
+  [ x' .>. ((-5)*^one)
+  , x' .<. (4*^one)
+  , y' .>. ((-5)*^one)
+  , y' .<. (3*^one ^-^ x')
+  , z' .>. ((-10)*^one)
+  , z' .<. (6*^one ^-^ (x' ^+^ y'))]
+  where
+    x = newVar 1
+    y = newVar 2
+    z = newVar 3
+    vars = [x,y,z]
+    x' = asLinearCombination vars x
+    y' = asLinearCombination vars y
+    z' = asLinearCombination vars z
+    one = constant 1
